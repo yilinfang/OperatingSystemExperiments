683f2b7dbbf65d2d1201f7b37942b2faf0af239b
yilinfang <935474206@qq.com>
2018-12-28 09:27:00 +0800

    finish exp3

--- exp3/3.c
+++ exp3/3.c
@@ -8,6 +8,8 @@
 #include <sys/file.h>
 #include <pthread.h>
 #include <wait.h>
+#include <memory.h>
+#include <string.h>
 #define P(x)          \
     {                 \
         sem_wait(x); \
@@ -17,10 +19,10 @@
         sem_post(x); \
     }
 
-sem_t *sem1, *sem2, *sem3;
+sem_t *sem1, *sem2;
 pid_t pid1, pid2;
-int shmid_b, shmid_s, shmid_e, shmid_sem1, shmid_sem2, shmid_sem3;
-int *start, *end;
+int shmid_b, shmid_s, shmid_e, shmid_sem1, shmid_sem2, shmid_status;
+int *start, *end, *status;
 char* buffer;
 int main(int argc, char const *argv[])
 {
@@ -28,78 +30,77 @@ int main(int argc, char const *argv[])
     {
         return 0;
     }
-    FILE *input = fopen(argv[1], "r");
-    FILE *output = fopen(argv[2], "w");
-    shmid_b = shmget(IPC_PRIVATE, 8, IPC_CREAT | 0666);
+    FILE *input = fopen(argv[1], "rb");
+    FILE *output = fopen(argv[2], "wb");
+    shmid_b = shmget(IPC_PRIVATE, 8 * 513 * sizeof(char), IPC_CREAT | 0666);
     shmid_s = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0666);
     shmid_e = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0666);
+    shmid_status = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0666);
     shmid_sem1 = shmget(IPC_PRIVATE, sizeof(sem_t), IPC_CREAT | 0666);
     shmid_sem2 = shmget(IPC_PRIVATE, sizeof(sem_t), IPC_CREAT | 0666);
-    shmid_sem3 = shmget(IPC_PRIVATE, sizeof(sem_t), IPC_CREAT | 0666);
     buffer = (char*)shmat(shmid_b, 0, 0);
     start = (int*)shmat(shmid_s, 0, 0);
     end = (int*)shmat(shmid_e, 0, 0);
+    status = (int*)shmat(shmid_status, 0, 0);
     sem1 = (sem_t*)shmat(shmid_sem1, 0, 0);
     sem2 = (sem_t*)shmat(shmid_sem2, 0, 0);
-    sem3 = (sem_t*)shmat(shmid_sem3, 0, 0);
     *start = 0;
     *end = 0;
     sem_init(sem1, 1, 0);
-    sem_init(sem2, 1, 8);
-    sem_init(sem3, 1, 1);
+    sem_init(sem2, 1, 7);
+    *status = 1;
     if ((pid1 = fork()) == 0)
     {
-        while (1)
+        char block[513];
+        while (*status || *start != *end)
         {
             P(sem1);
-            if (*(buffer + *start) == '#')
-            {
-                return 1;
-            }
-            fprintf(output,"%c",*(buffer + *start));
-            P(sem3);
-            printf("1:%c\n", *(buffer + *start));
-            V(sem3);
+            memcpy(block, buffer + *start * 513, 513 * sizeof(char));
+            fwrite(block, 1, strlen(block), output);
+            //printf("%s", block);
+            //sleep(0.5);
             *start = (*start + 1) % 8;
             V(sem2);
         }
+        return 1;
     }
     else if ((pid2 = fork()) == 0)
     {
-        char c;
-        fscanf(input,"%c", &c);
-        while (c != '#')
+        int c;
+        char block[513];
+        c = fread(block, 1, 512, input);
+        block[c] = '\0';
+        while (c == 512)
         {
             P(sem2);
-            *(buffer + *end) = c;
-            P(sem3);
-            printf("2:%c\n", *(buffer + *end));
-            V(sem3);
+            memcpy(buffer + *end * 513, block, 513 * sizeof(char));
             *end = (*end + 1) % 8;
             V(sem1);
-            fscanf(input, "%c", &c);
+            c = fread(block, 1, 512, input);
+            block[c] = '\0';
+            //printf("%d\n", c);
         }
         P(sem2);
-        *(buffer + *end) = c;
+        memcpy(buffer + *end * 513, block, 513 * sizeof(char));
         *end = (*end + 1) % 8;
         V(sem1);
+        *status = 0;
         return 2;
     }
-    int status;
-    wait(&status);
-    printf("end:%d\n", status);
-    wait(&status);
-    printf("end:%d\n", status);
+    int end;
+    wait(&end);
+    printf("end:%d\n", end);
+    wait(&end);
+    printf("end:%d\n", end);
     sem_destroy(sem1);
     sem_destroy(sem2);
-    sem_destroy(sem3);
     fclose(input);
     fclose(output);
     shmctl(shmid_b, IPC_RMID, 0);
     shmctl(shmid_s, IPC_RMID, 0);
     shmctl(shmid_e, IPC_RMID, 0);
+    shmctl(shmid_status, IPC_RMID, 0);
     shmctl(shmid_sem1, IPC_RMID, 0);
     shmctl(shmid_sem2, IPC_RMID, 0);
-    shmctl(shmid_sem3, IPC_RMID, 0);
     return 0;
 }
