091a278ec73d7e0514733c01550e9368dee3a910
yilinfang <935474206@qq.com>
2018-12-28 10:58:12 +0800

    change the buffer format

--- exp3/3.c
+++ exp3/3.c
@@ -33,16 +33,17 @@ int main(int argc, char const *argv[])
     FILE *output = fopen(argv[2], "wb");
     sem_t *sem1, *sem2;
     pid_t pid1, pid2;
-    int shmid_b, shmid_s, shmid_e, shmid_sem1, shmid_sem2, shmid_status;
+    int shmid_b[BUFFER_SIZE], shmid_s, shmid_e, shmid_sem1, shmid_sem2, shmid_status;
     int *start, *end, *status;
-    char *buffer;
-    shmid_b = shmget(IPC_PRIVATE, BUFFER_SIZE * (BLOCK_SIZE + 1) * sizeof(char), IPC_CREAT | 0666);
+    for(int i = 0; i < BUFFER_SIZE; i++)
+    {
+        shmid_b[i] = shmget(IPC_PRIVATE, (BLOCK_SIZE + 1) * sizeof(char), IPC_CREAT | 0666);
+    }
     shmid_s = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0666);
     shmid_e = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0666);
     shmid_status = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0666);
     shmid_sem1 = shmget(IPC_PRIVATE, sizeof(sem_t), IPC_CREAT | 0666);
     shmid_sem2 = shmget(IPC_PRIVATE, sizeof(sem_t), IPC_CREAT | 0666);
-    buffer = (char *)shmat(shmid_b, 0, 0);
     start = (int *)shmat(shmid_s, 0, 0);
     end = (int *)shmat(shmid_e, 0, 0);
     status = (int *)shmat(shmid_status, 0, 0);
@@ -50,16 +51,18 @@ int main(int argc, char const *argv[])
     sem2 = (sem_t *)shmat(shmid_sem2, 0, 0);
     *start = 0;
     *end = 0;
+    *status = 1;
     sem_init(sem1, 1, 0);
     sem_init(sem2, 1, BUFFER_SIZE - 1);
-    *status = 1;
     if ((pid1 = fork()) == 0)
     {
         char block[BLOCK_SIZE + 1];
+        char* buffer;
         while (*status || *start != *end)
         {
             P(sem1);
-            memcpy(block, buffer + *start * (BLOCK_SIZE + 1), (BLOCK_SIZE + 1) * sizeof(char));
+            buffer = (char *)shmat(shmid_b[*start], 0, 0);
+            memcpy(block, buffer, (BLOCK_SIZE + 1) * sizeof(char));
             fwrite(block, 1, strlen(block), output);
             *start = (*start + 1) % BUFFER_SIZE;
             V(sem2);
@@ -69,20 +72,23 @@ int main(int argc, char const *argv[])
     else if ((pid2 = fork()) == 0)
     {
         int c;
+        char *buffer;
         char block[BLOCK_SIZE + 1];
         c = fread(block, 1, BLOCK_SIZE, input);
         block[c] = '\0';
         while (c == BLOCK_SIZE)
         {
             P(sem2);
-            memcpy(buffer + *end * (BLOCK_SIZE + 1), block, (BLOCK_SIZE + 1) * sizeof(char));
+            buffer = (char *)shmat(shmid_b[*end], 0, 0);
+            memcpy(buffer, block, (BLOCK_SIZE + 1) * sizeof(char));
             *end = (*end + 1) % BUFFER_SIZE;
             V(sem1);
             c = fread(block, 1, BLOCK_SIZE, input);
             block[c] = '\0';
         }
         P(sem2);
-        memcpy(buffer + *end * (BLOCK_SIZE + 1), block, (BLOCK_SIZE + 1) * sizeof(char));
+        buffer = (char *)shmat(shmid_b[*end], 0, 0);
+        memcpy(buffer, block, (BLOCK_SIZE + 1) * sizeof(char));
         *end = (*end + 1) % BUFFER_SIZE;
         V(sem1);
         *status = 0;
@@ -95,7 +101,10 @@ int main(int argc, char const *argv[])
     sem_destroy(sem2);
     fclose(input);
     fclose(output);
-    shmctl(shmid_b, IPC_RMID, 0);
+    for(int i = 0; i < BUFFER_SIZE; i++)
+    {
+        shmctl(shmid_b[i], IPC_RMID, 0);
+    }
     shmctl(shmid_s, IPC_RMID, 0);
     shmctl(shmid_e, IPC_RMID, 0);
     shmctl(shmid_status, IPC_RMID, 0);
